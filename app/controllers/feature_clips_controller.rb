a902231a767cd5872b3306dbf1ba23e22840bd62
aizazafridi <aizooafridi@gmail.com>
2019-09-28 00:09:47 +0000

    Feature Clips CSS updated

--- app/controllers/feature_clips_controller.rb
+++ app/controllers/feature_clips_controller.rb
@@ -1,9 +1,34 @@
 class FeatureClipsController < InheritedResources::Base
+    
+    layout 'admin'
+    
+    def index
+        @feature_clips = FeatureClip.paginate(:page => params[:page], :per_page => 20).newest
+    end
+    
+    def show
+        @feature_clip = FeatureClip.find(params[:id])
+        @clip = Clip.find_by_id(@feature_clip.clip_id)
+    end
+    
+    def new
+        @feature = FeatureClip.new
+    end
+    
+    def create
+        @feature = FeatureClip.new(feature_clip_params)
+        if @feature.save
+            redirect_to(feature_clips_path)
+        else
+            render('new')
+        end
+    end
+    
 
   private
 
     def feature_clip_params
-      params.require(:feature_clip).permit(:description)
+      params.require(:feature_clip).permit(:description, :clip_id)
     end
 
 end
