adbe53daeffda9247dc28f3e1a0a8b39b09a3bda
aizazafridi <aizooafridi@gmail.com>
2019-10-01 23:04:48 +0000

    basic layout added for home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
+#Adding mystyle to asset pipeline
+Rails.application.config.assets.precompile += %w( mystyle.css )
+
 Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
