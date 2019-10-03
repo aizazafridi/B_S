3fc124efbf5443ef09d31302b8714ef1b31dc060
aizaz <aizooafridi@gmail.com>
2019-10-03 11:13:55 -0230

    Featured Actress and Clips updated in homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
     layout "home"
-    
+
   def index
+    #Finding feature actress
+    @feature_actress = FeatureActress.order('created_at').last
+    @actress = Actress.find_by_id(@feature_actress.actress_id)
+    #Finding feature clip
+    @feature_clip = FeatureClip.order('created_at').last
+    @clip = Clip.find_by_id(@feature_clip.clip_id)
   end
 
   def browse_ac
