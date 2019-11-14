47f3064f5c93d60b765d61cdbc631cb48e940093
aizazafridi <aizooafridi@gmail.com>
2019-11-14 23:58:16 +0000

    clip view updated with vimeo integration

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -4,15 +4,15 @@ class Clip < ApplicationRecord
   has_many :feature_clips
  
   has_attached_file :image_url
-  has_attached_file :clip_src
+  #has_attached_file :clip_src
 
   scope :newest, lambda {order("created_at DESC")}
 
   validates_attachment :image_url,
                      content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
-  validates_attachment :clip_src,
-                    content_type: { content_type: ["video/mp4"] }
+  #validates_attachment :clip_src,
+  #                  content_type: { content_type: ["video/mp4"] }
 
 
 
